05dfdae62895c5c0f8483df1dd58ce31d13d75c1
Harry Chen <44143868+Harry7557558@users.noreply.github.com>
2021-11-13 19:09:29 -0500

    model fruits

--- isp-life/fruit_02_sdf.glsl
+++ isp-life/fruit_02_sdf.glsl
@@ -58,14 +58,8 @@ float sdEllipsoid(vec3 p, vec3 r) {
     return k1*(k1-1.0)/k2;
 }
 float sdTorus(vec3 p, float R, float r) {
-    vec2 q = vec2(length(p.xy)-R,p.z);
+    vec2 q = vec2(length(p.xz)-R,p.y);
     return length(q)-r;
-}
-float sdLnNormEllipsoid(vec3 p, vec3 r, float n) {
-    p = abs(p);
-    float k1 = pow(dot(pow(p/r,vec3(n)),vec3(1)),1.0/n);
-    float k2 = pow(dot(pow(p/(r*r),vec3(n)),vec3(1)),1.0/n);
-    return k1*(k1-1.0)/k2;
 }
 
 // smoothed blending functions
@@ -90,96 +84,44 @@ vec4 smin(vec4 a, vec4 b, float k) {
     float d = mix(b.w, a.w, h) - k * h * (1.0 - h);
     return vec4(mix(b.xyz, a.xyz, h), d);
 }
-vec4 smax(vec4 a, vec4 b, float k) {
-    return smin(a*vec4(1,1,1,-1), b*vec4(1,1,1,-1), k) * vec4(1,1,1,-1);
-}
-
-
-// noise functions, hash functions from https://www.shadertoy.com/view/4djSRW
-vec2 hash22(vec2 p) {
-    vec3 p3 = fract(vec3(p.xyx) * vec3(.1031, .1030, .0973));
-    p3 += dot(p3, p3.yzx+33.33);
-    return fract((p3.xx+p3.yz)*p3.zy);
-}
-vec3 hash33(vec3 p3) {
-	p3 = fract(p3 * vec3(.1031, .1030, .0973));
-	p3 += dot(p3, p3.yxz + 33.33);
-	return fract((vec3(p3.x, p3.x, p3.y) + vec3(p3.y, p3.z, p3.z))*p3.zyx);
-}
-float GradientNoise2D(vec2 xy) {
-    float i0 = floor(xy.x), i1 = i0 + 1.0;
-    float j0 = floor(xy.y), j1 = j0 + 1.0;
-    float v00 = dot(2.0 * hash22(vec2(i0, j0)) - 1.0, xy - vec2(i0, j0));
-    float v01 = dot(2.0 * hash22(vec2(i0, j1)) - 1.0, xy - vec2(i0, j1));
-    float v10 = dot(2.0 * hash22(vec2(i1, j0)) - 1.0, xy - vec2(i1, j0));
-    float v11 = dot(2.0 * hash22(vec2(i1, j1)) - 1.0, xy - vec2(i1, j1));
-    float xf = xy.x - i0; xf = xf * xf * xf * (10.0 + xf * (-15.0 + xf * 6.0));
-    float yf = xy.y - j0; yf = yf * yf * yf * (10.0 + yf * (-15.0 + yf * 6.0));
-    return v00 + (v10 - v00)*xf + (v01 - v00)*yf + (v00 + v11 - v01 - v10) * xf*yf;
-}
-float SimplexNoise3D(vec3 xyz) {
-	const float K1 = 0.3333333333;
-	const float K2 = 0.1666666667;
-	vec3 p = xyz + (xyz.x + xyz.y + xyz.z)*K1;
-	vec3 i = floor(p);
-	vec3 f0 = xyz - (i - (i.x + i.y + i.z)*K2);
-	//vec3f e = step(f0.yzx(), f0);  // possibly result in degenerated simplex
-	vec3 e = vec3(f0.y > f0.x ? 0.0 : 1.0, f0.z >= f0.y ? 0.0 : 1.0, f0.x > f0.z ? 0.0 : 1.0);
-	vec3 i1 = e * (vec3(1.0) - e.zxy);
-	vec3 i2 = vec3(1.0) - e.zxy * (vec3(1.0) - e);
-	vec3 f1 = f0 - i1 + K2;
-	vec3 f2 = f0 - i2 + 2.0*K2;
-	vec3 f3 = f0 - 1.0 + 3.0*K2;
-	vec3 n0 = 2.0 * hash33(i) - 1.0;
-	vec3 n1 = 2.0 * hash33(i + i1) - 1.0;
-	vec3 n2 = 2.0 * hash33(i + i2) - 1.0;
-	vec3 n3 = 2.0 * hash33(i + 1.0) - 1.0;
-	vec4 v = vec4(dot(f0, n0), dot(f1, n1), dot(f2, n2), dot(f3, n3));
-	vec4 w = max(-vec4(dot(f0, f0), dot(f1, f1), dot(f2, f2), dot(f3, f3)) + 0.5, vec4(0.0));
-	return dot((w*w*w*w) * v, vec4(32.0));
-}
 
 
 
-// Modeling
 
 vec4 mapFruit(vec3 p, bool col_required) {
-    p *= vec3(1.0,1.05,1.0);
-    float r = length(p.xy), a = atan(p.y, p.x), b = atan(r, p.z);
+    p *= vec3(1.05, 1.0, 1.0);
+    p += vec3(0,0,0.5);
+    float r = length(p.xy), a = atan(p.y, p.x);
     float x, y; vec3 q;
-    q = vec3(length(vec2(r,0.02))*cossin(asin(0.9*sin(2.5*a-0.5))/2.5), p.z);
-    float bottom_hole = -smin(
-            sdLnNormEllipsoid(q-vec3(0,0,-1.0), vec3(0.3,0.3,0.3), 5.0),
-            sdTorus(q-vec3(0,0,-0.7), 0.04, 0.05),
-            0.05);
-    vec4 d = vec4(0.05,0.7,0.9, smax(
-        sdEllipsoid(q, vec3(1.15,1.15,1.0-0.4/(1.0+10.0*length(vec3(q.xy,0.2)))))
-            + 0.02*sin(5.0*p.x)*sin(5.0*p.y)*sin(5.0*p.z),
-        bottom_hole, 0.02));
+    // fruit
+    q = vec3(r*cossin(asin(0.99*sin(2.5*a+0.3*sin(4.0*a)))/2.5), p.z);
+    vec4 fruit = vec4(0,0,0, sdEllipsoid(q-vec3(0.4,0,0.68), vec3(1.0,0.8,0.85+0.05*r)));
     if (col_required) {
-        const vec3 c1 = vec3(0.15,0.25,0.4);
-        const vec3 c2 = vec3(0.3,0.45,0.6);
-        const vec3 c3 = vec3(0.8,0.75,0.85);
-        float t = dot(p, vec3(0.5,0.5,1)) + 0.8*SimplexNoise3D(1.5*p);
-        d.xyz = mix(mix(c1, c2, 0.5+0.5*tanh(1.0*t)), c3, 0.5+0.5*tanh(1.0*(t-1.0)));
-        t = 0.4*GradientNoise2D(vec2(6.0*a,3.0*b))-4.0*(b/PI-0.5)+0.5;
-        d.xyz *= smootherstep(0.7+0.3*tanh(1.2*t));
+        fruit.xyz = mix(vec3(0.55,0.2,0.0),vec3(0.95,0.85,0.0),
+            smootherstep(0.4*(q.z+1.0*(r-0.9)-0.0+smax(q.z-1.2,0.0,0.1))
+                +0.012*sin(20.0*p.x)*sin(20.0*p.y)*sin(20.0*p.z))
+        );
     }
-    d = smin(d, vec4(0.5*vec3(0.1,0.12,0.15),
-        sdTorus(q-vec3(0,0,-0.92), 0.33, 0.05)),
-        0.05);
-    q = vec3(length(vec2(r,0.02))*cossin(asin(0.999*sin(2.5*a-0.5))/2.5), p.z);
-    d = smin(d, vec4(0.5*vec3(0.25,0.3,0.4),
-        smax(
-            sdLnNormEllipsoid(roty(-1.1)*(q-vec3(0.2+0.2/(1.0+10.0*length(vec2(q.y,0.05))),0,-0.8)), vec3(0.3,0.25,0.05), 2.5),
-            bottom_hole, 0.02)),
-        0.08);
-    if (col_required) {
-        d.xyz *= 0.2+0.8*smootherstep(10.0*sdTorus(p-vec3(0,0,-0.78),0.18,0.08));
-        d.xyz = tanh(d.xyz * vec3(0.7,1.1,1.3));
-    }
-    d = smin(d, vec4(0.75,0.75,0.5,
-        sdCapsule(q-vec3(0,0,0.8), 0.8, 0.2+0.05*sin(4.0*q.z))), 0.05);
+    q = vec3(r*cossin(asin(0.99*sin(2.5*a+0.8))/2.5), p.z);
+    fruit = smin(fruit, vec4(  // hair
+        mix(vec3(0.1,0.02,0.06),vec3(0.25,0.02,0.0),smootherstep(r/0.3)),
+        sdEllipsoid(roty(0.2-0.05*cos(3.0*a))*(q-vec3(0.08,0,1.54)), vec3(0.2+0.03*sin(4.0*a),0.12,0.05))), 0.05);
+    // sepal/stem
+    q = vec3(r*cossin(asin(0.98*sin(2.5*a-1.2))/2.5), p.z);
+    q -= vec3(0.2,0,0);
+    vec3 br;
+    br.x = 1.0+0.1*sin(3.0*a);
+    br.y = 1.2*(0.3-0.2*exp(-sqr(1.8*(q.x-1.0)))+0.2*exp(-sqr(4.0*(q.x-0.7))));
+    br.z = 0.1*pow(smax(br.y,0.0,0.1),0.2);
+    br *= 1.2;
+    q = roty(0.3)*(q-vec3(0.2,0,-0.25));
+    q.z += 0.05*sin(6.0*q.x);
+    vec4 d1 = vec4(0.4,0.35,0.0, sdEllipsoid(q, br));  // sepals
+    q = vec3(r*cossin(asin(0.95*sin(2.5*a-0.9))/2.5), p.z);
+    vec4 d2 = vec4(0.25,0.15,0.05, sdCapsule(q-vec3(0.05,0,-0.6), 1.0, 0.1));  // stem
+    vec4 disk = smin(d1, d2, 0.1);
+    // put them together
+    vec4 d = smin(disk, fruit, 0.03);
     return d;
 }
 
